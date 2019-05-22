93a2415649678338858e39e0c564ecce0a119af1
SOFTWARE <Rigoberto318722@gmail.com>
2019-05-21 20:54:01 -0500

    avanze gestionar fabricantes

--- CelularesWeb/Controllers/FabricantesController.cs
+++ CelularesWeb/Controllers/FabricantesController.cs
@@ -29,5 +29,26 @@ namespace CelularesWeb.Controllers
             }
                 return View(IstFabricante);
         }
+        public ActionResult Add()
+
+        {
+            Fabricantes_dto fabricantes_Dto = new Fabricantes_dto();
+            List<DOMINIO> lstPaises = null;
+
+            using (TIENDASEntities db = new TIENDASEntities())
+            {
+                lstPaises = (from d in db.DOMINIOS
+                             where d.TIPO_DOMINIO == "paises"
+                             orderby d.VLR_DOMINIO
+                             select  new DOMINIO
+                             {
+                                 ID_DOMINIO = d.TIPO_DOMINIO,
+                                 VLR_DOMINIO = d.VLR_DOMINIO
+                             }).ToList();
+                fabricantes_Dto.lstPaises = lstPaises;
+                   
+            }
+                return View();
+        }
     }
 }
--- CelularesWeb/Models/DTOs/Fabricantes_dto.cs
+++ CelularesWeb/Models/DTOs/Fabricantes_dto.cs
@@ -2,14 +2,21 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace CelularesWeb.Models.DTOs
 {
     public class Fabricantes_dto
     {
         public decimal IdFabricante{get; set;}
+        [Required] 
+        [Display(Name ="Nombre fabricante")]
         public String NomFabricante { get; set; }
+        [Required]
+        [Display(Name = "Pais fabricante")]
         public String PaisFabricante { get; set; }
+
+        public List<DOMINIO> lstPaises { get; set; }
 
 
     }
